c927c86b59518524874403b72d2fc045dbe1db15
Eddie Jaoude <eddie@jaoudestudios.com>
2018-04-07 12:00:21 +0100

    Ship shoots with soundfx

--- src/Level1.js
+++ src/Level1.js
@@ -2,26 +2,34 @@ class Level1 extends Phaser.Scene {
 
     constructor() {
         super({ key: 'Level1' });
-    }
 
+        this.firing = false;
+    }
 
     preload() {
         this.load.image('sky', 'src/assets/sky.jpg');
-        this.load.spritesheet('ship', 'src/assets/ships.gif', { frameWidth: 34, frameHeight: 65 });
+        this.load.image('ship', 'src/assets/kenney_spaceshooterextension/PNG/Sprites/Ships/spaceShips_001.png');
+        this.load.image('missile', 'src/assets/kenney_spaceshooterextension/PNG/Sprites/Missiles/spaceMissiles_001.png');
+        this.load.audio('fire', 'src/assets/soundfx/gameburp/TECH WEAPON Gun Shot Phaser Down 02.wav');
     }
 
     create() {
+        console.log(this);
         this.add.image(0, 100, 'sky');
-
-        this.platforms = this.physics.add.staticGroup();
-
-        this.player = this.physics.add.sprite(100, 450, 'ship');
+        this.player = this.physics.add.sprite(Math.floor(Math.random() * Math.floor(400)) + 50, Math.floor(Math.random() * Math.floor(500)) + 50, 'ship');
+        this.player.angle = -90;
+        this.player.setDisplaySize(50, 50);
         this.player.setBounce(0.5);
         this.player.setCollideWorldBounds(true);
 
         this.cursors = this.input.keyboard.createCursorKeys();
-    }
 
+        this.fire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.input.keyboard.addKeyCapture([Phaser.Input.Keyboard.KeyCodes.SPACE]);
+
+        this.fireSound = this.sound.add('fire');
+        this.fireSound.volume = 0.1;
+    }
 
     update() {
         if (this.cursors.left.isDown) {
@@ -44,6 +52,17 @@ class Level1 extends Phaser.Scene {
             this.player.setVelocityY(0);
         }
 
+        if (this.fire.isDown) {
+            if (!this.firing) {
+                this.firing = true;
+                this.missile = this.physics.add.image(this.player.x + 30, this.player.y, 'missile');
+                this.missile.angle = 90;
+                this.missile.setVelocityX(200);
+                this.fireSound.play();
+            }
+        } else if (this.fire.isUp) {
+            this.firing = false;
+        }
         // if (cursors.up.isDown && player.body.touching.down)
         // {
         //     player.setVelocityY(-330);
